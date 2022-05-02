e2d0e15bf493e7912762cb63ee90aec542306046
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-02 17:22:15 +0200

    complete

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 $string = " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum hic dolor et suscipit? Repellendus, unde autem iure distinctio inventore repudiandae est quo expedita rerum fuga debitis, quam illum nihil dolorum! ";
 $badWords = $_GET['badWords'];
+$finalString = str_replace($badWords, '***', $string);
 ?>
 
 <!DOCTYPE html>
@@ -15,8 +16,8 @@ $badWords = $_GET['badWords'];
 </head>
 
 <body>
-    <h2> <?php echo str_replace($badWords, '***', $string) ?> </h2>
-    <h3> The lenght of the paragraph is <?php echo (strlen($string)) ?> characters long </h3>
+    <h2> <?php echo $finalString ?> </h2>
+    <h3> The paragraph is <?php echo (strlen($finalString)) ?> characters long </h3>
 
 </body>
 
