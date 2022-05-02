cd1c0fb3463469588aad4d7904a43cf3d4c80201
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-02 15:55:33 +0200

    exploring php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = "John";
+$age = "35";
+
+?>
+
+<!-- <h1> Hello, my name is <?php echo $name; ?>, and I'am <?php echo $age; ?> year old </h1> -->
---
+++ new.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = $_GET['name'];
+// $age = $argv[2];
+
+?>
+
+<h1>Hello <?php echo $name ?> </h1>
