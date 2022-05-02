18290f30952f6a18640b1248ace8a3b63c7c13c4
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-02 16:07:43 +0200

    added text

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php
 
-$name = "John";
-$age = "35";
+$string = " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum hic dolor et suscipit? Repellendus, unde autem iure distinctio inventore repudiandae est quo expedita rerum fuga debitis, quam illum nihil dolorum! ";
 
 ?>
 
 <!-- <h1> Hello, my name is <?php echo $name; ?>, and I'am <?php echo $age; ?> year old </h1> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <h2> <?php echo $string ?> </h2>
+
+</body>
+
+</html>
--- new.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$name = $_GET['name'];
-// $age = $argv[2];
-
-?>
-
-<h1>Hello <?php echo $name ?> </h1>
